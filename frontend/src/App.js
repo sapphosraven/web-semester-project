333d04a27fe50b1255e2e7c7dddc3ab98c7abcf8
Baigherat <mahabaig7@gmail.com>
2024-05-19 00:08:34 +0500

    made the necessary react components for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+
 import HomePage from './components/HomePage';
 import ArticlePage from './components/ArticlePage';
 import QuizPage from './components/QuizPage';
@@ -11,22 +16,21 @@ import SignInPage from './components/SignInPage';
 import SignUpPage from './components/SignUpPage';
 import ProfilePage from './components/ProfilePage';
 import NavBar from './components/NavBar';
-import './App.css';
 
 function App() {
   return (
     <Router>
       <NavBar />
       <div className="container mt-4">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/article/:id" component={ArticlePage} />
-          <Route path="/quiz/:id" component={QuizPage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInPage} />
-          <Route path="/signup" component={SignUpPage} />
-          <Route path="/profile" component={ProfilePage} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/article/:id" element={<ArticlePage />} />
+          <Route path="/quiz/:id" element={<QuizPage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
       </div>
     </Router>
   );
---
+++ frontend/src/components/QuizPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QuizPage = () => {
+  return <div>Quiz Page</div>;
+};
+
+export default QuizPage;
