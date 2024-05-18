6adc5a3d01dc4a9e491671c3c06375e6e33f0077
shahlizaahmed <shahlizaahmed27@gmail.com>
2024-05-18 23:08:42 +0500

    updated app.js in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ArticlePage from './components/ArticlePage';
+import QuizPage from './components/QuizPage';
+import ShopPage from './components/ShopPage';
+import SignInPage from './components/SignInPage';
+import SignUpPage from './components/SignUpPage';
+import ProfilePage from './components/ProfilePage';
+import NavBar from './components/NavBar';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container mt-4">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/article/:id" component={ArticlePage} />
+          <Route path="/quiz/:id" component={QuizPage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/profile" component={ProfilePage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
