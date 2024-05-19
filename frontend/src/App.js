6d2edf00a54c0d186eef429861aa086c52a84d0f
sapphosraven <113235622+sapphosraven@users.noreply.github.com>
2024-05-19 18:48:03 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,19 +14,16 @@ import LoginPage from "./components/LoginPage";
 function App() {
   return (
     <div className="App">
-
       <Router>
         <TopNavbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="users/signup" component={SignUpPage} /> 
-          <Route path="users/signup" component={LoginPage} /> 
-          <Route path="/articles/:id" element={<ArticlePage />} />
-
+          <Route path="users/signup" component={SignUpPage} />
+          <Route path="users/signup" component={LoginPage} />
+          <Route path="/articles" element={<ArticlePage />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </Router>
-
     </div>
   );
 }
