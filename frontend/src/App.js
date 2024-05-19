295000900aec229ea8f6d421ff8ab0e9f3b0fa8a
Baigherat <mahabaig7@gmail.com>
2024-05-19 16:37:51 +0500

    imported signup page in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage"; 
 //import ArticlePage from "./components/ArticlePage";
 import TopNavbar from "./components/TopNavbar";
 //import CategoryPage from "./components/CategoryPage"; // Import your new component
 import Footer from "./components/BFooter";
+
 
 function App() {
   return (
