fe8ba7605882065898c9761fef5f00495c661764
Baigherat <mahabaig7@gmail.com>
2024-05-19 16:55:00 +0500

    merging conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,11 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-<<<<<<< HEAD
+
 import SignUpPage from "./components/SignUpPage";
 import LoginPage from "./components/LoginPage"; 
 //import ArticlePage from "./components/ArticlePage";
-=======
-import ArticlePage from "./components/ArticlePage";
-import SignUpPage from "./components/SignUpPage";
->>>>>>> 1f50318cac55623b2b844d28706b1458420a53eb
+
 import TopNavbar from "./components/TopNavbar";
 //import CategoryPage from "./components/CategoryPage"; // Import your new component
 import Footer from "./components/BFooter";
